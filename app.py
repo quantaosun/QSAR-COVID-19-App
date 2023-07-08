037ef4457990c0211580949c33727d1c25fa8f4a
Quantao <75652473+quantaosun@users.noreply.github.com>
2023-07-08 23:28:31 +1000

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import pickle
 def desc_calc():
     # Performs the descriptor calculation
     bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     output, error = process.communicate()
     os.remove('molecule.smi')
 
