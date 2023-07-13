d3bd78ee07062ef21cde24c0a8cec5ed98959bd9
Quantao <75652473+quantaosun@users.noreply.github.com>
2023-07-13 19:15:52 +1000

    Update app.py

--- app.py
+++ app.py
@@ -41,10 +41,10 @@ st.image(image, use_column_width=True)
 
 # Page title
 st.markdown("""
-# 基于机器学习模型的新冠小分子抑制剂活性预测 (3C-like protease)
+# 基于机器学习模型的新冠小分子抑制剂活性预测
 
-- This application is designed to predict the bioactivity of molecules inhibiting the COVID-19 3C-like protein target. Out of the initial 10,086 small molecule data points downloaded from the Chembl database, only 657 had readily available IC50 values. To enhance the reliability of the model, IC50 values between 1,000 and 10,000 nanomolar were excluded due to concerns about the accuracy and reproducibility of the biology assay. This filtering process resulted in a refined dataset of 353 data points, which were used to build a random forest prediction model correlating molecular descriptors with IC50 values.
-- 该应用程序旨在预测抑制COVID-19 3C-like蛋白靶点的分子的生物活性。从Chembl数据库中下载了10,086个小分子数据点，其中只有657个具有可用的IC50值。为了提高模型的可靠性，排除了1,000至10,000纳摩尔之间的IC50值，原因是对生物学测定的准确性和重复性存在疑虑。这一筛选过程导致了353个数据点的精选数据集，用于构建分子描述符和IC50值之间的随机森林预测模型。
+- This application is designed to predict the bioactivity of molecules inhibiting the COVID-19 virus, not a single protein. Out of the initial 10,086 small molecule data points downloaded from the Chembl database, only 657 had readily available IC50 values. To enhance the reliability of the model, IC50 values between 1,000 and 10,000 nanomolar were excluded due to concerns about the accuracy and reproducibility of the biology assay. This filtering process resulted in a refined dataset of 353 data points, which were used to build a random forest prediction model correlating molecular descriptors with IC50 values.
+- 该应用程序旨在预测抑制COVID-19病毒的分子的生物活性。从Chembl数据库中下载了10,086个小分子数据点，其中只有657个具有可用的IC50值。为了提高模型的可靠性，排除了1,000至10,000纳摩尔之间的IC50值，原因是对生物学测定的准确性和重复性存在疑虑。这一筛选过程导致了353个数据点的精选数据集，用于构建分子描述符和IC50值之间的随机森林预测模型。
 
 **Credits**
 - Built and deployed by [Quantao Sun](https://github.com/quantaosun)
@@ -91,5 +91,5 @@ else:
 
     # Logo image
 image = Image.open('regression.png')
-st.header('**Training set 353 datapoints distribution **')
+st.header('Model built by training set ')
 st.image(image, use_column_width=True)
